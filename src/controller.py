379a1aab0e6f747e11d37405e8f0d0b4e594835c
sergiorosadoferrer <sergio_rf6@uma.es>
2018-12-03 17:02:54 +0100

    nuevo

---
+++ src/controller.py
@@ -0,0 +1,92 @@
+from suds.client import Client
+import comic
+import entrega
+import requests
+import json
+
+
+
+class Controller(object):
+    wsdl=''
+    service = 'http://localhost:8080/restFullHumilArt/webresources/entity.comic/'
+    
+   
+
+    def findComicById(self, idComic):
+        c = requests.get(service+str(idComic))
+        c=json.loads(c.text)
+        print c
+        if c:
+            cam = comic.Comic(c['idComic'],c['nombre'],c['descripcion'],c['fechaCreacion'])
+            return cam
+        else:
+            return None
+
+    def addComic(self,nombre,descripcion):
+        requests.post(service+nombre+'/'+descripcion)
+    def deleteComic(self,comic):
+        self.client.service.remove(comic)
+    def editComic(self, comic, nuevoNombre,nuevaDescripcion):
+        self.client.service.editComic(comic,nuevoNombre,nuevaDescripcion)
+
+    def listComics(self):
+        listaComics = []
+        lista = requests.get('http://localhost:8080/restFullHumilArt/webresources/entity.comic/findAll')
+        lista=json.loads(lista.text)
+        for i in range(len(lista)):
+            comi = comic.Comic(lista[i]['idComic'],lista[i]['nombre'],lista[i]['descripcion'],lista[i]['fechaCreacion'])
+            listaComics.append(comi)
+        return listaComics
+    
+    def listaOrden(self):
+        aux=[]
+        lista=self.client.service.encontrarPorNombreAlfabetico();
+        for i in range(len(lista)):
+            comi = comic.Comic(lista[i]['idComic'],lista[i]['nombre'],lista[i]['descripcion'],lista[i]['fechaCreacion'])
+            aux.append(comi)
+        return aux
+    
+    def listaFecha(self):
+        aux=[]
+        lista=self.client.service.encontrarPorFecha();
+        for i in range(len(lista)):
+            comi = comic.Comic(lista[i]['idComic'],lista[i]['nombre'],lista[i]['descripcion'],lista[i]['fechaCreacion'])
+            aux.append(comi)
+        return aux
+    
+    def listaNombre(self,nombre):
+        aux=[]
+        lista=self.client.service.buscarNombre(nombre);
+        for i in range(len(lista)):
+            comi = comic.Comic(lista[i]['idComic'],lista[i]['nombre'],lista[i]['descripcion'],lista[i]['fechaCreacion'])
+            aux.append(comi)
+        return aux
+    
+    def listaFechaMayor(self,fecha):
+        aux=[]
+        lista=self.client.service.buscarPorFechaMayor(fecha);
+        for i in range(len(lista)):
+            comi = comic.Comic(lista[i]['idComic'],lista[i]['nombre'],lista[i]['descripcion'],lista[i]['fechaCreacion'])
+            aux.append(comi)
+        return aux
+    
+    def listaNumEntregas(self):
+        aux=[]
+        lista=requests.get(service)
+        for i in range(len(lista)):
+            comi = comic.Comic(lista[i]['idComic'],lista[i]['nombre'],lista[i]['descripcion'],lista[i]['fechaCreacion'])
+            aux.append(comi)
+        return aux
+    
+    def getEntregasComic(self,comic):
+        aux=[]
+        lista=requests.get('http://localhost:8080/restFullHumilArt/webresources/entity.entrega/filtraEntregaComic/'+str(comic))
+        lista=json.loads(lista.text)
+        for i in range(len(lista)):
+            entregas=entrega.Entrega(lista[i]['idEntrega'],lista[i]['nombre'],lista[i]['archivo'],lista[i]['fechaCreacion'],lista[i]['idComic'])
+            aux.append(entregas)
+        return aux
+        
+    
+    
+        
